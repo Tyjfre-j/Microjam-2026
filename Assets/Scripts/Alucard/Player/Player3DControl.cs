41da52687454794ab93c565b33a16f6f454e5ced
bouhamza abderrahmane <bouhamzaabouda@gmail.com>
2026-03-27 15:55:02 +0100

    Migrate input to new Input System

--- Assets/Scripts/Alucard/Player/Player3DControl.cs
+++ Assets/Scripts/Alucard/Player/Player3DControl.cs
@@ -1,41 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player3DControl : MonoBehaviour
 {
-    public float moveSpeed = 7f;
-    public float jumpForce = 10f;
-    public LayerMask walkableLayer;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask walkableLayer;
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
 
     private Rigidbody rb;
     private bool isGrounded;
+    private float moveInput;
+    private bool jumpRequested;
 
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void OnEnable()
     {
-     
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (moveAction != null) { moveAction.action.Enable(); }
+        if (jumpAction != null) { jumpAction.action.Enable(); }
+    }
+
+    private void OnDisable()
+    {
+        if (moveAction != null) { moveAction.action.Disable(); }
+        if (jumpAction != null) { jumpAction.action.Disable(); }
+    }
+
+    private void Update()
+    {
+        if (moveAction != null)
         {
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            Vector2 move = moveAction.action.ReadValue<Vector2>();
+            moveInput = move.x;
+        }
+
+        if (jumpAction != null && jumpAction.action.WasPressedThisFrame())
+        {
+            jumpRequested = true;
         }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        
         isGrounded = Physics.Raycast(transform.position, -transform.up, 1.2f, walkableLayer);
 
-      
-        float h = Input.GetAxis("Horizontal");
-        
-        Vector3 moveDir = transform.right * h * moveSpeed;
+        if (jumpRequested && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+        jumpRequested = false;
 
-        
         Vector3 localVelocity = transform.InverseTransformDirection(rb.linearVelocity);
-        localVelocity.x = h * moveSpeed;
+        localVelocity.x = moveInput * moveSpeed;
         rb.linearVelocity = transform.TransformDirection(localVelocity);
     }
 }

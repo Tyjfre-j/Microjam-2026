c177486cbd687080cffcec91685fc0754d6d65dc
bouhamza abderrahmane <bouhamzaabouda@gmail.com>
2026-03-28 20:55:50 +0100

    Add GameManager for game state management and rotation completion handling

--- Assets/Scripts/Cube/Controllers/RotationButton.cs
+++ Assets/Scripts/Cube/Controllers/RotationButton.cs
@@ -29,7 +29,7 @@ public class RotationButton : MonoBehaviour
     {
         if (rotationAnimator == null)
         {
-            rotationAnimator = FindObjectOfType<RotationAnimator>();
+            rotationAnimator = FindAnyObjectByType<RotationAnimator>();
         }
     }
 
